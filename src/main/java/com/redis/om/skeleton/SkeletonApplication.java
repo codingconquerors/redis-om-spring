0c09f22ca71b78f7bc78ab3d2acac6835f602a54
khushwinder <chahalkhushwinder@gmail.com>
2023-02-16 21:06:14 -0500

    Initial version

--- src/main/java/com/redis/om/skeleton/SkeletonApplication.java
+++ src/main/java/com/redis/om/skeleton/SkeletonApplication.java
@@ -1,34 +1,26 @@
 package com.redis.om.skeleton;
 
-import java.time.Instant;
-import java.util.List;
-import java.util.Set;
-
+import com.redis.om.skeleton.models.Address;
+import com.redis.om.skeleton.models.Person;
+import com.redis.om.skeleton.repositories.PeopleRepository;
+import com.redis.om.spring.annotations.EnableRedisDocumentRepositories;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springdoc.core.models.GroupedOpenApi;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.geo.Point;
 
-import com.redis.om.skeleton.models.Address;
-import com.redis.om.skeleton.models.Person;
-import com.redis.om.skeleton.repositories.PeopleRepository;
-import com.redis.om.spring.annotations.EnableRedisDocumentRepositories;
-
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Set;
 
 @SpringBootApplication
 @EnableRedisDocumentRepositories(basePackages = "com.redis.om.skeleton.*")
 public class SkeletonApplication {
-
-    @Autowired
-    PeopleRepository repo;
 
     @Bean
     CommandLineRunner loadTestData(PeopleRepository repo) {
@@ -38,12 +30,12 @@ public class SkeletonApplication {
             String thorSays = "The Rabbit Is Correct, And Clearly The Smartest One Among You.";
 
             // Serendipity, 248 Seven Mile Beach Rd, Broken Head NSW 2481, Australia
-            Address thorsAddress = Address.of("248", "Seven Mile Beach Rd", "Broken Head", "NSW", "2481", "Australia");
+            Address thorsAddress = Address.of("23", "Seven Mile Beach Rd", "Broken Head", "NSW", "2481", "Australia");
             DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
             String date = "2017-12-03T10:15:30+01:00";
             OffsetDateTime date1 = OffsetDateTime.parse(date, dateTimeFormatter);
 
-            Instant inst = Instant.parse("2017-02-03T10:37:30.00Z");
+            // Instant inst = Instant.parse("2017-02-03T10:37:30.00Z");
 
             Person thor = Person.of("Chris", "Hemsworth", 38, thorSays, new Point(153.616667, -28.716667), thorsAddress,
                     Set.of("hammer", "biceps", "hair", "heart"), date1);
